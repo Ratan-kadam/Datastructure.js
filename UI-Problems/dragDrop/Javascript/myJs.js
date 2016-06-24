baf52d2f5872db8dcfd17f1e14caddc0af6ced48
Ratan kadam <ratan.kadam@gmail.com>
2016-06-24 11:43:11 -0700

    DragDrop example

--- UI-Problems/dragDrop/Javascript/myJs.js
+++ UI-Problems/dragDrop/Javascript/myJs.js
@@ -1,7 +1,20 @@
-var dragStarted=function(event){
-   console.log("x:" +event.clientX + "y:" + event.clientY);
-};
 
-var droppedHere = function(){
-  alert("dropped here");
-};
+function draggingStarted(event){
+    event.dataTransfer.setData("token",event.target.id);
+}
+
+function dropped(event){
+  event.preventDefault();
+     var itemsId =event.dataTransfer.getData("token");
+     alert(itemsId);
+     var cart = document.getElementById("cartID");
+   // event.target.appendChild()
+    cart.appendChild(document.getElementById(itemsId));
+
+    //console.log(cart);
+}
+
+function stopDefaultBehaviour(event){
+  event.preventDefault();
+}
+
