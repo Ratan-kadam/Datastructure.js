3da29b518b412b620196f01d88b6fe8e453eab2f
Ratan kadam <ratan.kadam@gmail.com>
2016-07-13 11:30:42 -0700

    Angular_form_validation

---
+++ comman_modules/Angular-form/app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/validate.html',
+        controller: 'validateCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
